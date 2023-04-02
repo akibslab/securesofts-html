b664db70d1d7a462bc65c02deb49f933d23ae2bf
iTanvir <thisistanvir.dev@gmail.com>
2023-04-02 12:05:35 +0600

    update works page

--- assets/js/main.js
+++ assets/js/main.js
@@ -200,89 +200,23 @@
 		});
 	}
 
-	// // WOW active
-	// var wow = new WOW({
-	// 	boxClass: "wow",
-	// 	animateClass: "animated",
-	// 	offset: 0,
-	// 	mobile: true,
-	// 	live: true,
-	// });
-	// wow.init();
-	// //  End Wow
+	// Start Init Isotope
+	var $grid = $("#works-grid").isotope({
+		itemSelector: ".grid-item",
+		percentPosition: true,
+	});
+	// filter items on button click
+	$(".filter_tabs").on("click", "button", function () {
+		var filterValue = $(this).attr("data-filter");
+		$grid.isotope({ filter: filterValue });
+	});
+	//for menu active class
+	$(".filter_tabs button").on("click", function (event) {
+		$(this).siblings(".active").removeClass("active");
+		$(this).addClass("active");
+		event.preventDefault();
+	});
+	// End IsoTope
 
-	// //  Scroll Up
-	// $.scrollUp({
-	// 	scrollName: "scrollUp",
-	// 	topDistance: "300",
-	// 	topSpeed: 300,
-	// 	animation: "fade",
-	// 	animationInSpeed: 200,
-	// 	animationOutSpeed: 200,
-	// 	scrollText: '<i class="fa fa-arrow-up"></i>',
-	// 	activeOverlay: false,
-	// });
-	// //   Scroll Up
-
-	// // Start Owl-carousel
-	// $(".case-study-carousel.style-2").owlCarousel({
-	// 	items: 3,
-	// 	loop: true,
-	// 	dots: true,
-	// 	thumbs: false,
-	// 	nav: true,
-	// 	navText: [
-	// 		'<i class="fa fa-angle-left"></i>',
-	// 		'<i class="fa fa-angle-right"></i>',
-	// 	],
-	// 	autoplay: true,
-	// 	autoplayTimeout: 1000,
-	// 	autoplayHoverPause: true,
-	// 	margin: 30,
-	// 	responsive: {
-	// 		0: {
-	// 			items: 1,
-	// 		},
-	// 		767: {
-	// 			items: 2,
-	// 		},
-	// 		992: {
-	// 			items: 3,
-	// 		},
-	// 	},
-	// });
-	// // End Owl-carousel //
-
-	// // Magnific Popup
-	// $(".video-play").magnificPopup({
-	// 	type: "iframe",
-	// });
-	// //   End  Magnific Popup
-
-	// //for menu active class
-	// $(".portfolio-tab button").on("click", function (event) {
-	// 	$(this).siblings(".active").removeClass("active");
-	// 	$(this).addClass("active");
-	// 	event.preventDefault();
-	// });
-
-	// // Smooth Scroll for IE/ EDGE/ SAFARI
-	// $("a").on("click", function (event) {
-	// 	if (this.hash !== "") {
-	// 		event.preventDefault();
-
-	// 		var hash = this.hash;
-
-	// 		$("html, body").animate(
-	// 			{
-	// 				scrollTop: $(hash).offset().top,
-	// 			},
-	// 			800,
-	// 			function () {
-	// 				window.location.hash = hash;
-	// 			}
-	// 		);
-	// 	}
-	// });
-	// // End Smooth Scroll for IE/ EDGE/ SAFARI
+	//  End Init IsoTope
 })(jQuery);
